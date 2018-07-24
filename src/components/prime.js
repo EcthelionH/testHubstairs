2b39a6f6697f2bf29a1759698c5e32fd35afd0c5
Hugo Vasquez Bullon <cuentas.hs@gmail.com>
2018-07-24 19:57:44 +0200

    prime component

--- src/components/prime.js
+++ src/components/prime.js
@@ -1,9 +1,66 @@
 import React, { Component } from 'react';
+import { Input, Button, TextField } from '@material-ui/core';
 
 class Prime extends Component {
+    constructor() {
+        super();
+        this.state = {
+            inputNumber: 0,
+            outputNumber: 0,
+        }
+    }
+
+    calculateNthPrimeNumber = (num) => {
+        var i = 1;
+        var c = 0;
+        var arr = [];
+        while (c < num){
+          i++;
+          if (this.isPrime(i)){
+            arr.push(i);
+            c++;
+          }
+        }
+        return arr[num-1];
+      }
+
+      isPrime = (num) => {        
+        for (var i=2; i < num; i++){
+          if (num % i === 0){
+            return false;
+          }
+        }
+        return true;
+      }
+
+    handleClick = () => {
+        const outputNumber = this.calculateNthPrimeNumber(this.state.inputNumber);
+        this.setState({ outputNumber });
+    }
+
+    handleChange = (event) => {
+        this.setState({inputNumber: event.target.value})
+    }
+
     render() {
         return (
-            <p>Prime</p>
+            <div>
+                <h3>Calculer le n-ième nombre premier</h3>
+                <div>
+                    <p>Tapez un nombre</p>
+                    <Input
+                        id='number'
+                        type='number'
+                        value={this.state.inputNumber}
+                        onChange={this.handleChange}
+                        />
+                    <Button onClick={this.handleClick} >Calculer</Button>
+                </div>
+                <div>
+                    <p>Le {this.state.inputNumber}ième nombre premier est</p>
+                    <TextField readOnly disabled value={this.state.outputNumber}/>
+                </div>
+            </div>
         )
     }
 }
